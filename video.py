d6cb1e41e1b878a769b42276390c5eb19de6d62f
fredrikskatvedt <72396026+fredrikskatvedt@users.noreply.github.com>
2023-05-22 17:17:18 +0200

    Update video.py

--- video.py
+++ video.py
@@ -5,13 +5,13 @@ import sys
 import ffmpeg
 import Clipping
 
+# Path to the JSON file containing metadata
+JSON_PATH = r'C:\Users\aulic\Downloads\Download-video_test 2\Download-video_test\videoer\1089\meta_1089.txt'
+# Path to the game file
+VIDEO_PATH = r'C:\Users\aulic\Downloads\Download-video_test 2\Download-video_test\videoer\1089\game_1089.mp4'
 
-file_path= r'C:\Users\aulic\Downloads\Download-video_test 2\Download-video_test\videoer\1089\meta_1089.txt'
-file_path2= r'C:\Users\aulic\Downloads\Download-video_test 2\Download-video_test\videoer\1089\game_1089.mp4'
 
-
-
-
+# Function to extract the start time of the video from the metadata file
 def startvideo2(infile):
     match = re.search(r'^#\s*Video start timestamp:\s*(.*?)\s*$', infile.readline())
 
@@ -20,26 +20,28 @@ def startvideo2(infile):
         date_time = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
         startvideo = date_time.strftime('%H:%M:%S')
         return startvideo
-    
 
+
+# Function to count the number of lines in a file
 def count_lines(filename):
     with open(filename) as f:
         return sum(1 for i in f)
-    
 
+
+# Function to format the JSON data and write it to a new file
 def formatjson():
-    with open(file_path, 'r') as infile, open('output.json', 'w') as outfile:
-        countLines = count_lines(file_path)
-        amount_lines = countLines-2
-        outfile.write("{")
+    with open(JSON_PATH, 'r') as infile, open('output.json', 'w') as outfile:
+        countLines = count_lines(JSON_PATH)
+        amount_lines = countLines - 2
         startvideo = startvideo2(infile)
+        outfile.write("{")
 
         for i, line in enumerate(infile):
-            line = line.replace("'", '"') 
+            line = line.replace("'", '"')
             line = line.replace(',"', ":")
             line = line.replace('"\n', "")
             outfile.write(line)
-            if i != amount_lines: 
+            if i != amount_lines:
                 outfile.write(",\n")
 
         outfile.write("}")
@@ -47,99 +49,98 @@ def formatjson():
         return startvideo
     
 
+def clipname(start_clip, config_action):
+    matchtime_str = str(start_clip)
+    formatString = matchtime_str.replace(':', "-")
+
+    output_filename = f"{config_action}_{formatString}.mp4"
+    return output_filename
+
+# Function to perform video clipping based on specified timings
 def clipTimings(config_action, key, config_start, config_stop):
-                
     timestamp = datetime.strptime(key, "%Y-%m-%d %H:%M:%S.%f")
     normaltime_start = timestamp - timedelta(seconds=config_start)
     start = datetime.strptime(normaltime_start.strftime('%H:%M:%S'), '%H:%M:%S').time()
     normaltime_end = timestamp + timedelta(seconds=config_stop)
     end = datetime.strptime(normaltime_end.strftime('%H:%M:%S'), '%H:%M:%S').time()
 
+    start_clip = datetime.combine(datetime.min, start) - datetime.combine(
+        datetime.min, datetime.strptime(startvideo, '%H:%M:%S').time())
+    end_clip = datetime.combine(datetime.min, end) - datetime.combine(
+        datetime.min, datetime.strptime(startvideo, '%H:%M:%S').time())
 
-    start_clip = datetime.combine(datetime.min, start) - datetime.combine(datetime.min, datetime.strptime(startvideo, '%H:%M:%S').time())
-    end_clip = datetime.combine(datetime.min, end) - datetime.combine(datetime.min, datetime.strptime(startvideo, '%H:%M:%S').time())
+    output_filename = clipname(start_clip, config_action)
 
-
-    matchtime_str = str(start_clip)
-    formatString = matchtime_str.replace(':', "-")
-
-    output_filename = f"{config_action}_{formatString}.mp4"
-
-    #Clipping.trim(file_path2, output_filename, start_clip, end_clip)
-    print("mongomann\n")
+    Clipping.trim(VIDEO_PATH, output_filename, start_clip, end_clip)
 
 
+def framerate(config_start, config_stop):
+    probe = ffmpeg.probe(VIDEO_PATH)
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+
+    if video_stream is None:
+        print('No video stream found')
+        exit(1)
+
+    frame_rate_str = video_stream['avg_frame_rate']
+    x, y = frame_rate_str.split('/')
+    frame_rate = int(x) / int(y)
+    frames_bef = config_start / frame_rate
+    frames_aft = config_stop / frame_rate
+
+    return frames_bef, frames_aft
 
 
+def duration(config_start, config_stop):
+    duration = config_start / 2
+    duration2 = config_start / 4
+    duration3 = duration + duration2
 
+    start_time = [0, duration2, duration, duration3, config_start][config_stop - 1]
+    end_time = [config_start, duration3, duration, duration2, 0][config_stop - 1]
+    return start_time, end_time
+
+
+# Function to process different types of video events
 def videoTypes(config_action, cutType, numEvents, config_start, config_stop):
-
-
     i = 0
     for key in data:
         if data[key]["action"] == config_action:
-            
-            
-            if cutType == 1:       #cuttype seconds
-                
+            if cutType == 1:  # cuttype seconds
+                i += 1
                 clipTimings(config_action, key, config_start, config_stop)
 
-
-            elif cutType == 2:     #cuttype frames
-
-                probe = ffmpeg.probe(file_path2)
-                video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-
-                if video_stream is None:
-                    print('No video stream found')
-                    exit(1)
-
-                frame_rate_str = video_stream['avg_frame_rate']
-                x, y = frame_rate_str.split('/')
-                frame_rate = int(x) / int(y)
-                frames_bef = config_start/frame_rate
-                frames_aft = config_stop/frame_rate
-
+            elif cutType == 2:  # cuttype frames
+                i += 1
+                frames_bef, frames_aft = framerate(config_start, config_stop)
                 clipTimings(config_action, key, frames_bef, frames_aft)
 
-
-            elif cutType == 3:     #cuttype duration
-
-
-                duration = config_start / 2
-                duration2 = config_start / 4
-                duration3 = duration + duration2
-
-                start_time = [0, duration2, duration, duration3, config_start][config_stop - 1]
-                end_time = [config_start, duration3, duration, duration2, 0][config_stop - 1]
-
-                print(start_time, end_time)
-                clipTimings(config_action, key, start_time, end_time)
-
-
+            elif cutType == 3:  # cuttype duration
                 i += 1
-                if i == numEvents:
-                    break
-
-
+                start_time, end_time = duration(config_start, config_stop)
+                clipTimings(config_action, key, start_time, end_time)
+        
+        if i == numEvents:
+            break
 
 if __name__ == '__main__':
-
+    # Validate command-line arguments
     if (len(sys.argv) not in [6] or
-        sys.argv[1] not in ['goal', 'red_card', 'yellow_card', 'free_kick', 'shot', 'offside'] or
-        sys.argv[2] not in ['1', '2', '3'] or
-        not sys.argv[3].isdigit() or
-        not sys.argv[4].isdigit() or
-        not sys.argv[5].isdigit() or
-        sys.argv[2] == 3 and not sys.argv[5] == ['1', '2', '3', '4', '5']):
-        
-            print('Usage: {} [goal|red_card|yellow_card|free_kick|shot] [1|2|3] [must number, antall] [must number, før] [must number, etter]'.format(sys.argv[0]))
-            exit(1)
-
-
-
-
-
+            sys.argv[1] not in ['goal', 'red_card', 'yellow_card', 'free_kick', 'shot', 'offside', 'substitution', 'corner','penalty'] or
+            sys.argv[2] not in ['1', '2', '3'] or
+            not sys.argv[3].isdigit() or
+            not sys.argv[4].isdigit() or
+            not sys.argv[5].isdigit() or
+            sys.argv[2] == '3' and not sys.argv[5] == ['1', '2', '3', '4', '5']):
+        if sys.argv[2] == 3:
+            print('Usage: {} [goal|red_card|yellow_card|free_kick|shot|substitution|corner|penalty]'
+            ' [1|2|3] [number of events] [amount of duration] [1|2|3|4|5]'
+            .format(sys.argv[0]))
+        else :  
+            print('Usage: {} [goal|red_card|yellow_card|free_kick|shot|substitution|corner|penalty]'
+            ' [1|2|3] [number of events] [amount of seconds before] [amount of seconds after]'
+            .format(sys.argv[0]))
+        exit(1)
 
     eventType = sys.argv[1].replace('_', ' ')
     cutType = int(sys.argv[2])
@@ -153,6 +154,3 @@ if __name__ == '__main__':
         data = json.load(f)
 
     videoTypes(eventType, cutType, numEvents, secondsBef, secondsAft)
-
-
-
